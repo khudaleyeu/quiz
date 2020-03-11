85efb9d191dd0311cf2828d5fb663aa407d9f74b
ykhudaleyeu <e.hudoleev@gmail.com>
2020-03-11 11:14:49 +0300

    update: add fullscreen mode method

--- app/src/main/java/com/khudaleyeu/quiz/MainActivity.kt
+++ app/src/main/java/com/khudaleyeu/quiz/MainActivity.kt
@@ -2,11 +2,25 @@ package com.khudaleyeu.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toggleFullScreen()
+    }
+
+    fun toggleFullScreen() {
+        if (window.decorView.systemUiVisibility == View.SYSTEM_UI_FLAG_VISIBLE) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN
+        } else {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        }
     }
 }
