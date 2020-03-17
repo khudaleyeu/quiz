c41da9c58af1361eff462e3eab7c9bae0961eaf1
ykhudaleyeu <e.hudoleev@gmail.com>
2020-03-17 18:07:54 +0300

    update: add start button action in MainActivity.kt, set popup call on hardware back button action

--- app/src/main/java/com/khudaleyeu/quiz/MainActivity.kt
+++ app/src/main/java/com/khudaleyeu/quiz/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.khudaleyeu.quiz
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.Gravity
 import android.view.View
-import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toggleFullScreen()
+        buttonStart.setOnClickListener {
+                val intent = Intent(this, GameLevels::class.java)
+                startActivity(intent)
+        }
     }
 
     fun toggleFullScreen() {
@@ -23,4 +31,24 @@ class MainActivity : AppCompatActivity() {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
         }
     }
+    override fun onBackPressed(){
+        var backPressedTime = System.currentTimeMillis() + 2000
+        when (backPressedTime > System.currentTimeMillis()) {
+            false -> super.onBackPressed()
+            true -> {
+                var toast : Toast = Toast.makeText(baseContext, "Press twice to exit!", Toast.LENGTH_SHORT)
+                toast.setGravity(Gravity.BOTTOM, 0, getAbsDisplayHeight())
+                var toastView : View = toast.view
+                toastView.setBackgroundResource(R.drawable.toast_drawable);
+                toast.show();
+            }
+        }
+    }
+
+    fun getAbsDisplayHeight():Int{
+       val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.heightPixels / 9
+    }
+
 }
